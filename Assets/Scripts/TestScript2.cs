2838934a3c418a1cd88dd8ce54b846c373112ed2
albertgalicinskiy <galicinskiyalba@mail.ru>
2023-07-05 14:28:17 +0500

    Searching and Finding GameObjects in Unity

--- Assets/Scripts/TestScript2.cs
+++ Assets/Scripts/TestScript2.cs
@@ -4,9 +4,36 @@ using UnityEngine;
 
 public class TestScript2 : MonoBehaviour
 {
+
+    GameObject searchObj; // if I want to do something with object I have to store it in variables
+
+    GameObject[] enemies;
+
     // Start is called before the first frame update
     void Start()
     {
+        /* Search using element name
+
+        // searchObj = GameObject.Find("Sphere"); // find object in Unity
+        searchObj = GameObject.Find("Cube/Cube2/Enemy"); // find object inside another object
+        Destroy(searchObj);
+
+        */
+
+        // Search using element with Tag 
+        
+        
+        // searchObj = GameObject.FindWithTag("Enemy"); // Destroy only one tagged element
+
+        // enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        /*
+        foreach (GameObject enemy in enemies)
+        {
+            Destroy(enemy); // Destroy all elements with tag Enemy one by one
+        }
+        */
+        GameObject.Find("Cube").SetActive(false);
         
     }
 
