f678bb80ba2cfd0aa27201226fc7330993dd5cfa
albertgalicinskiy <galicinskiyalba@mail.ru>
2023-07-06 17:57:09 +0500

    Repeatedly calling Functions with Invoke Repeting Learn Invoke, InvokeRepeating and CancelInvoke Functions

--- Assets/Scripts/InstantiatingTest.cs
+++ Assets/Scripts/InstantiatingTest.cs
@@ -15,6 +15,10 @@ public class InstantiatingTest : MonoBehaviour
         // and our ball will appear at the same position with cube because our script is attached to it
         // Box Colider should be Off for Cube and for the ball Prefab we have to add Rigidbody in this case
         //Instantiate(ball, transform.position, Quaternion.identity); // Quaternion.identity = we don't want any rotation
+    
+        //Invoke("RandomBall", 5f); // after 5s running the game RandomBall function will be called
+    
+        InvokeRepeating("RandomBall", 2f, 1f); // after 2s running the game this piece of code will start executing and function will be called after each 1 sec
     }
 
     // Update is called once per frame
@@ -23,9 +27,18 @@ public class InstantiatingTest : MonoBehaviour
         // If we click left button of mouse our ball will appear
         if ( Input.GetMouseButtonDown (0)) { // 0 = left button of mouse
             
-            int randomNumber = Random.Range(0, ball.Length); // 0, 3
-            
-            Instantiate(ball[randomNumber], transform.position, Quaternion.identity);
+            // int randomNumber = Random.Range(0, ball.Length); // 0, 3
+            // Instantiate(ball[randomNumber], transform.position, Quaternion.identity);
+
+            print("Mouse is Clicked");
+            CancelInvoke("RandomBall"); // stop calling function when we click left mouse button
         }
+    }
+
+    void RandomBall()
+    {
+        int randomNumber = Random.Range(0, ball.Length); // 0, 3
+            
+        Instantiate(ball[randomNumber], transform.position, Quaternion.identity);
     }
 }
